539f8db6dbd15ba7c05d8c5f5556f17015c743d2
amit <amit@mac2.local>
2021-02-02 12:53:21 +0530

    test

--- src/App.js
+++ src/App.js
@@ -1,7 +1,51 @@
 import logo from './logo.svg';
 import './App.css';
+import { createRef } from "react";
+
+
+
 
 function App() {
+
+  const p = createRef();
+
+  function timeout(ms) {
+    return new Promise(resolve => setTimeout(resolve, ms));
+  }
+
+  const getResult = async () => {
+    await timeout(3000);
+    if (5 > 3) {
+      throw new Error("HI");
+    }
+    return "HI";
+  }
+
+  const getRemoteMedia = async () => {
+    alert("STARTED");
+    const a6 = 5;
+    try {
+      await new Promise(async (resolve, reject) => {
+
+        const p2 = async (e) => {
+          alert(a6);
+
+          try {
+            const result = await getResult();
+            alert(result);
+          } catch (error) {
+            console.error('Error in subscribing - (timeout)' + error);
+            reject(error);
+          }
+        };
+        document.addEventListener('click', p2, false);
+
+      });
+    } catch (error) {
+      console.error('Error in onicecandidate - (timeout2):' + error);
+    }
+  }
+
   return (
     <div className="App">
       <header className="App-header">
@@ -9,14 +53,12 @@ function App() {
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+        <div
+          onClick={getRemoteMedia}
+          ref={p}
         >
           Learn React
-        </a>
+        </div>
       </header>
     </div>
   );
